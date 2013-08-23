cc111cc6eef652012364f4b5b1b5196185d959d2
Judith Hengeveld <judith@hengeveld.com>
2013-08-23 13:20:08 -0700

    cleanned up javascript into asset file, removed loggers, added default route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,17 +6,12 @@ class GamesController < ApplicationController
 
   def take_turn
     game = Game.new(params.select{|k,v| k.match(/^grid/)}.values)
-    Rails.logger.info("<><><><><><><><>BOARD=#{game.board}")
-    Rails.logger.info("<><><><><><><><>BOARD_STR=#{game.send(:board_string)}")
     if (winner = game.turn) == "draw" && (turns_left = game.turns_left) > 0
        @turns_left = turns_left
     else
       @winner = winner
       @turns_left = 0 
     end
-    Rails.logger.info("<><><><><><><><>WINNER=#{winner.inspect}")
-    Rails.logger.info("<><><><><><><><>@WINNER=#{@winner.inspect}")
-    #render :json => @winner
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ TicTacToe::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'games#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
