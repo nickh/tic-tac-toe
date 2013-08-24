2fb06642b9c085455de661026180915cfc8ea14f
Judith Hengeveld <judith@hengeveld.com>
2013-08-23 22:19:09 -0700

    turn off turbolinks as it was causing the javascript not to load on
    'Play Again', use $(document).ready instead of window.onload, and changed
    hard coded link to generated link_to for 'Play Again'

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+$(document).ready(function() {
   $("select[id^='grid']").change(function() {
 		$('#ttt_form').submit();
 		$('input#'+$(this).attr('id')).val($(this).val());
 		$('input#'+$(this).attr('id')).prop('disabled', false);
 		$(this).prop('disabled', true);
   });
-};
+});
